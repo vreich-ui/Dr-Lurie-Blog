a1370d1e3999e54444a15426a0324da76bd2e91f
vreich-ui <vreich@kugelbrands.com>
2026-07-07 17:44:46 +0300

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -8,7 +8,7 @@ export const headerData = {
       href: getPermalink('/'),
       links: [
         {
-          text: 'Home',
+          text: 'Homer',
           href: getPermalink('/'),
           description: 'A science-first overview of what changes in skin after 60.',
         },
